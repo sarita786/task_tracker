de170d19116d01bf8dee748480fc5e53799db9db
sarita <saritamc786@gmail.com>
2021-02-08 20:41:26 +1100

    Add Task + Delete Task + Fetch all task

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import {Link} from 'react-router-dom'
+const Footer = () => {
+    return (
+        <footer>
+            <p>Copyright & Copy @2021</p>
+            <a href='/about'>About</a>
+
+        </footer>
+
+
+    )
+}
+
+export default Footer
